3fd06f274dcc56dbd4573672d55d3a32359016dc
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-08-05 08:37:06 +0900

    Initial Commit

---
+++ src/main/java/com/deefacto/alim_service/AlimServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deefacto.alim_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deefacto/alim_service/AlimServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deefacto.alim_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
