b7983b3b639732baefd69008c61aeac881ccc304
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-08-13 10:42:27 +0900

    feat | sprint2 | LC-128 | [Kafka] NotificationUser 위한 User 정보 받는 remote 통신 구현 | SHUN
    - Kafka 의존성 주입 및 설정 완료
    - Kafka DTO (request, response)
    - Kafka Producer: notification 정보 전달
    - Kafka Consumer: user 정보 수신
    
    수신받은 user 정보 기반으로 NotificationUser 저장

---
+++ src/main/java/com/deefacto/alim_service/remote/service/UserResponseConsumer.java
@@ -0,0 +1,26 @@
+package com.deefacto.alim_service.remote.service;
+
+import com.deefacto.alim_service.commonNoti.service.NotificationUserService;
+import com.deefacto.alim_service.remote.dto.UserMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserResponseConsumer {
+
+    private final NotificationUserService notificationUserService;
+
+    @KafkaListener(
+            topics = "user.response",
+            groupId = "notification-service-group",
+            properties = {
+                    JsonDeserializer.VALUE_DEFAULT_TYPE + ":com.deefacto.alim_service.remote.dto.UserMessage$UserResponseMessage"
+            }
+    )
+    public void consumeUserResponse(UserMessage.UserResponseMessage response) {
+        notificationUserService.assignNotificationToUsers(response.getNotificationId(), response.getUserIds());
+    }
+}
